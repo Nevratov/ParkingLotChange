281b870f7ebd8c423c9c7099f88d9aa271dcc3b5
Maxim Nevratov <nevratov22@gmail.com>
2023-05-16 23:27:00 +0300

    Complete 4 stage

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,8 +3,13 @@ fun main() {
 
     do {
         val request = readln().split(" ")
-        if ("park" in request) parking.parkCar(request[1], request[2])
-        else if ("leave" in request) parking.leaveCar(request[1].toInt())
 
+        if (parking.spots.isEmpty() && request[0].lowercase() != "create") {
+            println("Sorry, a parking lot has not been created.")
+            continue
+        } else if ("park" in request) parking.parkCar(request[1], request[2])
+        else if ("leave" in request) parking.leaveCar(request[1].toInt())
+        else if ("status" in request) parking.status()
+        else if ("create" in request) parking.create(request[1].toInt())
     } while (request[0].lowercase() != "exit")
 }
